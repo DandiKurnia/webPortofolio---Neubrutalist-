8d7c0cc8ee1b3d909b0ae3e5356342a6e38c2af0
Dandi Kurnia <dandikurnia@gmail.com>
2026-05-13 13:56:16 +0700

    fix auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -62,17 +62,7 @@ export const authOptions: NextAuthOptions = {
   jwt: {
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
-  cookies: {
-    sessionToken: {
-      name: `next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: process.env.NODE_ENV === "production",
-      },
-    },
-  },
+
   pages: {
     signIn: "/admin/login",
   },
